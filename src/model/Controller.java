9fdcfab5fcd351450f219b033a1c3e9e1293abea
Studioname <83342697+Studioname@users.noreply.github.com>
2022-07-05 11:00:13 +0100

    Update Controller.java

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -22,12 +22,26 @@ public class Controller {
 			//welcome user, press a key to continue
 			
 			//menu screen
-			String[] menu = {"1. Search all shows", "2. Search by Name", "3. Search by Date", "4. Purchase Ticket"};
-			for (int i = 0; i < menu.length; i++) {
+			String[] defaultMenu = {"1. Search all shows", "2. Search by Name", "3. Search by Date",};
+			for (int i = 0; i < defaultMenu.length; i++) {
 				System.out.println("Menu choices");
 			}
 			int userInput = inputReader.getNextInt();
 			inputReader.nextLine();
+			switch (userInput) {
+			//get all plays
+			case 1: dbm.printResult(dbm.getPlays());
+			//not yet implemented
+			case 2: 
+				//submenu - press 1 to add to basket, 2 to return to main menu
+				break;
+			//not yet implemented
+			case 3: 
+				//submenu - press 1 to add to basket, 2 to return to main menu
+				break;
+			case 4: 
+				//
+			}
 			//submenus
 			
 			

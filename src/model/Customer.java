bf0c005f5e270f0b1bc4071b1b2921a372dbb7df
Studioname <83342697+Studioname@users.noreply.github.com>
2022-07-05 15:24:09 +0100

    Introduced Customer class

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,58 @@
 package model;
 
 public class Customer {
+	private String customerName;
+	private String customerAddress;
+	private String customerTelephone;
+	private String customerEmail;
+	private String customerPaymentDetails;
+	
+	public Customer(String customerName,String customerAddress,String customerTelephone,String customerEmail,String customerPaymentDetails) {
+		this.customerName = customerName;
+		this.customerAddress = customerAddress;
+		this.customerTelephone = customerTelephone;
+		this.customerEmail = customerEmail;
+		this.customerPaymentDetails = customerPaymentDetails;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+	public String getCustomerTelephone() {
+		return customerTelephone;
+	}
+
+	public void setCustomerTelephone(String customerTelephone) {
+		this.customerTelephone = customerTelephone;
+	}
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerPaymentDetails() {
+		return customerPaymentDetails;
+	}
+
+	public void setCustomerPaymentDetails(String customerPaymentDetails) {
+		this.customerPaymentDetails = customerPaymentDetails;
+	}
 	
 }
